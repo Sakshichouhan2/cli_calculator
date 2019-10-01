4c36185643ba55f3b608a4f6330f3b51b310da54
Sakshichouhan2 <chouhansakshi88@gmail.com>
2019-10-01 13:24:20 +0530

    add exception handling code

--- main.py
+++ main.py
@@ -4,66 +4,85 @@ from single_operand import *
 
 opr_choice = input("for binary operand press 1 \ unary  operand press 2\n")
 
-if opr_choice == "1":
-	choice = input("please enter choice for performing operations press 1 for add\n press 2 for sub\n press 3 for div\n press 4 for mul\n press 5 for Exponentiation\n press 6 for modulus\n press 7 for floor_division\n")
-	operand1 = int(input("enter first operation :")) 
-	operand2 = int(input("enter second operation :")) 
+try:
 
-	if choice =="1":
-		z = add(operand1,operand2)
-		print("{} + {} = {}".format(operand1,operand2,z))
+	if opr_choice == "1":
+		choice = input("please enter choice for performing operations press 1 for add\n press 2 for sub\n press 3 for div\n press 4 for mul\n press 5 for Exponentiation\n press 6 for modulus\n press 7 for floor_division\n")
+		operand1 = int(input("enter first operation :")) 
+		operand2 = int(input("enter second operation :")) 
 
-	elif choice =="2":
-		z = sub(operand1,operand2)
-		print("{} - {} = {}".format(operand1,operand2,z))	
+		if choice =="1":
+			z = add(operand1,operand2)
+			print("{} + {} = {}".format(operand1,operand2,z))
 
-	elif choice =="3":
-		z = div(operand1,operand2)
-		print("{} / {} = {}".format(operand1,operand2,z))
-		
-	elif choice =="4":
-		z = mul(operand1,operand2)
-		print("{} * {} = {}".format(operand1,operand2,z)) 
+		elif choice =="2":
+			z = sub(operand1,operand2)
+			print("{} - {} = {}".format(operand1,operand2,z))	
 
-	elif choice =="5":
-		z = Exponentiation(operand1,operand2)
-		z = print("{} ** {} = {}".format(operand1,operand2,z))
+		elif choice =="3":
+			z = div(operand1,operand2)
+			print("{} / {} = {}".format(operand1,operand2,z)) 
 
-	elif choice =="6":
-		z = modulus(operand1,operand2)
-		z = print("{} % {} = {}".format(operand1,operand2,z))
 
-	elif choice =="7":
-		z = floor_division(operand1,operand2)
-		z = print("{} // {} = {}".format(operand1,operand2,z))
+		elif choice =="4":
+			z = mul(operand1,operand2)
+			print("{} * {} = {}".format(operand1,operand2,z)) 
 
+		elif choice =="5":
+			z = Exponentiation(operand1,operand2)
+			z = print("{} ** {} = {}".format(operand1,operand2,z))
+
+		elif choice =="6":
+			z = modulus(operand1,operand2)
+			z = print("{} % {} = {}".format(operand1,operand2,z))
+
+		elif choice =="7":
+			z = floor_division(operand1,operand2)
+			z = print("{} // {} = {}".format(operand1,operand2,z))
 	else: 
 		print("Invalid Choice")
-		
-if opr_choice =="2":
-	choice = input("please enter choice for performing operations press 1 for square\n press 2 for cube\n press 3 for square_root\n press 4 for cube_root\n press 5 for factorial\n")
-	operand1 = int(input("enter first number: "))
+			
+	if opr_choice =="2":
+		choice = input("please enter choice for performing operations press 1 for square\n press 2 for cube\n press 3 for square_root\n press 4 for cube_root\n press 5 for factorial\n")
+		operand1 = int(input("enter first number: "))
 
-	if choice == "1":
-		z = square(operand1)
-		print("square of {} = {}".format(operand1,z))
+		if choice == "1":
+			z = square(operand1)
+			print("square of {} = {}".format(operand1,z))
 
-	elif choice == "2":
-		z = cube(operand1)
-		print("cube of {} = {}".format(operand1,z))
+		elif choice == "2":
+			z = cube(operand1)
+			print("cube of {} = {}".format(operand1,z))
 
-	elif choice == "3":
-		z = square(operand1)
-		print("square_root of {} = {}".format(operand1,z))
+		elif choice == "3":
+			z = square(operand1)
+			print("square_root of {} = {}".format(operand1,z))
 
-	elif choice == "4":
-		z = square(operand1)
-		print("scube_root of {} = {}".format(operand1,z))
+		elif choice == "4":
+			z = square(operand1)
+			print("scube_root of {} = {}".format(operand1,z))
 
-	elif choice =="5":
-		z = factorial(operand1)
-		print("factorial of {} = {}".format(operand1,z))
+		elif choice =="5":
+			z = factorial(operand1)
+			print("factorial of {} = {}".format(operand1,z))
 
-	else:
-		print("Invalid Choice")
+		else:
+			print("Invalid Choice")
 
+except ZeroDivisionError:	
+	print("You can not divide a number by zero")
+
+except ValueError:
+	print("Enter an integer number")
+
+except NameError:
+	print("This name is not def")
+
+except TypeError:
+	print("Input given in string please provide valid inputs")
+
+except SyntaxError:
+	print("There is an SyntaxError")
+
+except IndentationError:
+	print("There is an IndentationError ")
